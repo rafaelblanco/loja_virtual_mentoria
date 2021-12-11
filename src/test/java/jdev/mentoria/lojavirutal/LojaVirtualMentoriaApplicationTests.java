3c08088f8bc842d342233bcff61913a1a515751a
User <User@User-PC>
2021-12-10 22:06:44 -0300

    commit inicial projeto loja virtual

---
+++ src/test/java/jdev/mentoria/lojavirutal/LojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package jdev.mentoria.lojavirutal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
